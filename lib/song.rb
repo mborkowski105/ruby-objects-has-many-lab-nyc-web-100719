fc7e787afe079d2abf1fb9dfb13b6c69a14d4e88
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-11 15:50:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,10 +18,10 @@ class Song
   end
   
   def artist_name
-    if (self.artist.name)
+    if (self.artist)
       return self.artist.name
     else 
-      return nil
-    end
+      return nil 
+      
   end
 end
