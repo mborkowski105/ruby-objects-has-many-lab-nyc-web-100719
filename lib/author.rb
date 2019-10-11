fa8f722903042820f51d2fb71c33074255f4c1bf
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-11 15:57:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,28 @@
-class Artist
+require 'pry'
+
+class Author
   attr_reader :name
+  
   def initialize(name)
     @name = name 
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def posts
+    Posts.all.select do |post|
+      post.author 
+    end
+  end
+  
+  def add_post_by_title(name)
+    post = Post.new(name)
+    self.add_post(post)
+  end
+  
+  def self.post_count 
+    return Post.all.count
   end
 end
